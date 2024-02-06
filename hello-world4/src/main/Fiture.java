285e6287e0a102367debd98d48ef86ccf5705c65
AlfitoDwi <titodwimirza@gmail.com>
2024-02-06 13:32:02 +0700

    Tamabhan fitur

---
+++ hello-world4/src/main/Fiture.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Alfito Dwi
+ */
+public class Fiture {
+    public static void main(String[] args) {
+        System.out.println("oi");
+    }
+}
